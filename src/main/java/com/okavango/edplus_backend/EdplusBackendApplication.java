d98bc15f3122de402646987d4dd091f3bf8b2dc8
okavango <eguilhermeleitedev81@gmail.com>
2025-01-14 15:20:51 -0300

    created

---
+++ src/main/java/com/okavango/edplus_backend/EdplusBackendApplication.java
@@ -0,0 +1,15 @@
+package com.okavango.edplus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdplusBackendApplication
+    {
+
+        public static void main(String[] args)
+            {
+                SpringApplication.run(EdplusBackendApplication.class, args);
+            }
+
+    }
---
+++ src/test/java/com/okavango/edplus_backend/EdplusBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.okavango.edplus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdplusBackendApplicationTests
+    {
+
+        @Test
+        void contextLoads()
+            {
+            }
+
+    }
